079a9cb0dccd52d395b9a0ef52b685f7603959e0
linjw1008 <linjw1008@163.com>
2019-08-27 21:37:37 +0800

    二叉树最大深度

---
+++ recursion/maximum_depth_of_binary_tree.h
@@ -0,0 +1,26 @@
+#ifndef _MAXIMUM_DEPTH_OF_BINARY_TREE_H_
+#define _MAXIMUM_DEPTH_OF_BINARY_TREE_H_
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+#endif
