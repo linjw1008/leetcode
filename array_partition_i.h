4932e8ab11a6c3e67cea74d6a05f4b570e35458c
linjw1008 <linjw1008@163.com>
2020-04-01 19:53:59 +0800

    数组拆分

---
+++ array_partition_i.h
@@ -0,0 +1,14 @@
+#include "common.h"
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int len = nums.size();
+        int sum = 0;
+        for (int i = 0; i < len; i = i + 2)
+            sum += nums[i];
+        
+        return sum;
+    }
+};
